9abfa3986948058ccbf33bcd0335b3c6b3456ffe
DaniGarcia2007 <158280740+DaniGarcia2007@users.noreply.github.com>
2024-01-31 12:35:28 +0100

    Create BATERIA1-2.py

---
+++ BATERIA1-2.py
@@ -0,0 +1,5 @@
+# Programa que pregunta el nom de l'usuari i només dona la benvinguda si l'usuari es diu "Amilcar" o "Sila".
+# Programa que pregunta un número a l'usuari i diu si és major o menor que 100.
+# Programa que pregunta un número a l'usuari i diu si és senar o parell.
+# Programa que pregunta la nota a l'usuari i segons la nota diu el resultat obtingut: 1 a 4 - Insuficient, 5 - Suficient, 6 - Bé, 7 a 8 - Notable, 9 a 10 - Excel·lent.
+# Fes un programa que pregunti quants anys portes treballant de programador i et digui que ets junior si portes menys de 5 anys i senior si portes més de 5.
