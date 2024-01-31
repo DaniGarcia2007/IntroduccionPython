66097a07849af6dbff2b9e3f073dae1ff148046d
DaniGarcia2007 <158280740+DaniGarcia2007@users.noreply.github.com>
2024-01-31 12:35:22 +0100

    Create BATERIA1-3.py

---
+++ BATERIA1-3.py
@@ -0,0 +1,5 @@
+# Fes un programa que demana un número a l'usuari i imprimeix la seva taula de multiplicar (del 1 al 10).
+# Fes un programa que pregunti un número a l'usuari i retorni la suma de tots els números fins el que ha introduït l'usuari.
+# Modifica el programa anterior per a que només sumi els números parells.
+# Modifica el programa anterior per que sigui l'usuari si vol sumar els números parells o els senars.
+# Fes un programa que imprimeixi per pantalla els números primers.
