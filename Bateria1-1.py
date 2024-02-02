84fa23e344ae0547b121a2681c7601781757ad9d
DaniGarcia2007 <158280740+DaniGarcia2007@users.noreply.github.com>
2024-02-02 13:24:46 +0100

    Update Bateria1-1.py

--- Bateria1-1.py
+++ Bateria1-1.py
@@ -1,5 +1,5 @@
 # Programa que pregunta dos números a l'usuari i calcula la seva suma.
-# Programa que pregunta un número i retorna el seu quadrat.
-# Programa que pregunta el nom a l'usuari i retorna el text: "Hola X". On X és el nom de l'usuari.
-# Programa que demana un pes en kg a l'usuari i retorna el mateix pes convertit en lliures.
-# Programa que demana una temperatura en graus Celsius a l'usuari i retorna la temperatura en graus Fahrenheit.
+num1 = int(input("Introdueix un numero: ")
+num2 = int(input("Introdueix un altre numero: ")
+resultat = num1 + num2
+print = resultat
